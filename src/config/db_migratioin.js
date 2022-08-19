d940feeba829e1abe1e7346c2789735f6cfa5739
wisnuragaprawida <wisnuraga418@gmail.com>
2022-08-19 13:39:16 +0700

    [ADD] seed on db migration

--- src/config/db_migratioin.js
+++ src/config/db_migratioin.js
@@ -1,4 +1,17 @@
+const roleSeed = require('../model/seed/role.seed');
+const bookSeed = require('../model/seed/book.seed');
+const categorySeed = require('../model/seed/category.seed');
 const DbMigration = async (db) => {
-    db.squelize.sync();
+
+    const { sequelize, role,book , category} = db;
+    sequelize.sync({force:true}).then(() => {
+
+        roleSeed(role);
+        bookSeed(book);
+        categorySeed(category);
+        console.log("Drop and re-sync db.");
+
+
+    });
 }
 module.exports = DbMigration;
