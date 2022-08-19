ab429875c17bf1e93133e2c8c14f114d534e3f76
wisnuragaprawida <wisnuraga418@gmail.com>
2022-08-19 13:37:18 +0700

    [ADD] file category seed

---
+++ src/model/seed/category.seed.js
@@ -0,0 +1,24 @@
+module.exports = async (categoryModel)=>{
+
+    return await categoryModel.bulkCreate([
+         {name:'Novel'},
+         {name:'Cergram'},
+         {name:'Komik'},
+         {name:'ensiklopedia'},
+         {name:'nomik'},
+         {name:'antologi'},
+         {name:'dongeng'},
+         {name:'biografi'},
+         {name:'catatan Harian'},
+         {name:'fotografi'},
+         {name:'karya ilmiah'},
+         {name:'tafsir'},
+         {name:'kamus'},
+         {name:'panduan'},
+         {name:'atlas'},
+         {name:'buku ilmiah'},
+         {name:'buku teks'},
+         {name:'majalah'}
+     ]);
+ 
+ }
