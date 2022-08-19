0d57e06e744eca9f7fdb91e561d6cd4e5dc8fb53
Hadin Davidi Sianturi <theamevide@gmail.com>
2022-08-19 19:58:33 +0700

    [ADD] middleware to user

--- src/delivery/route/user.route.js
+++ src/delivery/route/user.route.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const roleMiddleware = require('../middleware/role.middleware');
 module.exports = (userController) => {
   const { create, list, findById, update } = userController();
-  router.post("/", create);
-  router.get("/register", create);
-  router.get("/", list);
-  router.get("/:id", findById);
-  router.put("/", update);
+  router.post("/", roleMiddleware().isAdmin, create);
+  router.get("/register", roleMiddleware().isAdmin, create);
+  router.get("/", roleMiddleware().isAdmin,list);
+  router.get("/:id", roleMiddleware().isAdmin, findById);
+  router.put("/", roleMiddleware().isAdmin, update);
 
   return router;
 };
