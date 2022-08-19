9e00f772d0b9f5fffbe5b5153e47b71f200ada05
Hadin Davidi Sianturi <theamevide@gmail.com>
2022-08-19 19:59:22 +0700

    [ADD] middleware to bookroute

--- src/delivery/route/book.route.js
+++ src/delivery/route/book.route.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth.middleware');
 const roleMiddleware = require('../middleware/role.middleware');
 module.exports = (bookController) => {
     const { create, list, findById, update, remove } = bookController();
-    router.post('/', create);
-    router.get('/', authMiddleware, roleMiddleware().isAdmin, list);
-    router.get('/:id', findById);
-    router.put('/', update);
-    router.delete('/:id', remove);
+    router.post('/', roleMiddleware().isAdmin, create);
+    router.get('/', list);
+    router.get('/:id', roleMiddleware().isAdmin, findById);
+    router.put('/', roleMiddleware().isAdmin, update);
+    router.delete('/:id', roleMiddleware().isAdmin, remove);
     return router;
 }
